38e7fdcb449c50316878dbc7fb8bbe8d0c88d16e
autom8ter <admin@autom8ter.com>
2019-04-29 17:17:00 -0600

    endpoints

--- README.md
+++ README.md
@@ -9,7 +9,6 @@
 
 ```go
 type Client struct {
-	Domain string `validate:"required"`
 }
 ```
 
@@ -17,7 +16,7 @@ type Client struct {
 #### func  NewClient
 
 ```go
-func NewClient(clientID string, clientSecret string, domain string, scopes []string) *Client
+func NewClient(ctx context.Context, clientID string, clientSecret string, domain string, scopes []string) *Client
 ```
 
 #### func (*Client) AddScopes
@@ -56,6 +55,12 @@ func (c *Client) Delete(uRL string) (*http.Response, error)
 func (c *Client) Do(req *http.Request) (*http.Response, error)
 ```
 
+#### func (*Client) Domain
+
+```go
+func (c *Client) Domain() string
+```
+
 #### func (*Client) Get
 
 ```go
@@ -80,6 +85,12 @@ func (c *Client) Post(uRL string, obj interface{}) (*http.Response, error)
 func (c *Client) PostForm(uRL string, formValues url.Values) (*http.Response, error)
 ```
 
+#### func (*Client) Scopes
+
+```go
+func (c *Client) Scopes() []string
+```
+
 #### func (*Client) SetContext
 
 ```go
@@ -96,4 +107,66 @@ func (c *Client) TokenURL() string
 
 ```go
 func (c *Client) Validate() error
+```
+
+#### type OAuth2Client
+
+```go
+type OAuth2Client struct {
+}
+```
+
+
+#### func  NewOAuth2Client
+
+```go
+func NewOAuth2Client(ctx context.Context, domain, clientID, clientSecret, redirect string, scopes []string) *OAuth2Client
+```
+
+#### func (*OAuth2Client) AuthURL
+
+```go
+func (c *OAuth2Client) AuthURL() string
+```
+
+#### func (*OAuth2Client) ClientID
+
+```go
+func (c *OAuth2Client) ClientID() string
+```
+
+#### func (*OAuth2Client) ClientSecret
+
+```go
+func (c *OAuth2Client) ClientSecret() string
+```
+
+#### func (*OAuth2Client) Context
+
+```go
+func (c *OAuth2Client) Context() context.Context
+```
+
+#### func (*OAuth2Client) Domain
+
+```go
+func (c *OAuth2Client) Domain() string
+```
+
+#### func (*OAuth2Client) HTTP
+
+```go
+func (c *OAuth2Client) HTTP(t *oauth2.Token) *http.Client
+```
+
+#### func (*OAuth2Client) Scopes
+
+```go
+func (c *OAuth2Client) Scopes() []string
+```
+
+#### func (*OAuth2Client) TokenURL
+
+```go
+func (c *OAuth2Client) TokenURL() string
 ```
--- auth0.go
+++ auth0.go
@@ -5,6 +5,7 @@ package auth0
 import (
 	"context"
 	"github.com/autom8ter/objectify"
+	"golang.org/x/oauth2"
 	"golang.org/x/oauth2/clientcredentials"
 	"net/http"
 	"net/url"
@@ -14,20 +15,21 @@ import (
 var util = objectify.Default()
 
 type Client struct {
-	Domain string `validate:"required"`
+	domain string `validate:"required"`
 	cfg *clientcredentials.Config
 	ctx context.Context
 }
 
-func NewClient(clientID string, clientSecret string, domain string, scopes []string) *Client {
+func NewClient(ctx context.Context, clientID string, clientSecret string, domain string, scopes []string) *Client {
 	return &Client{
-		Domain: domain,
+		domain: domain,
 		cfg: &clientcredentials.Config{
 			ClientID:     clientID,
 			ClientSecret: clientSecret,
-			TokenURL: "https://" + domain + "/oauth/token",
+			TokenURL:     "https://" + domain + "/oauth/token",
 			Scopes:       scopes,
 		},
+		ctx: ctx,
 	}
 }
 
@@ -100,4 +102,73 @@ func (c *Client) ClientSecret() string {
 
 func (c *Client) TokenURL() string {
 	return c.cfg.TokenURL
+}
+
+func (c *Client) Scopes() []string {
+	return c.cfg.Scopes
+}
+
+func (c *Client) Domain() string {
+	return c.domain
+}
+
+type OAuth2Client struct {
+	domain string
+	cfg    *oauth2.Config
+	ctx context.Context
+}
+
+func NewOAuth2Client(ctx context.Context, domain, clientID, clientSecret, redirect string, scopes []string) *OAuth2Client {
+	if ctx == nil {
+		ctx  = context.TODO()
+	}
+	return &OAuth2Client{
+		domain: domain,
+		cfg: &oauth2.Config{
+			ClientID:     clientID,
+			ClientSecret: clientSecret,
+			Endpoint: oauth2.Endpoint{
+				AuthURL:  "https://" + domain + "/authorize",
+				TokenURL: "https://" + domain + "/oauth/token",
+			},
+			RedirectURL:  redirect,
+			Scopes:       scopes,
+		},
+		ctx: ctx,
+	}
+}
+
+func (c *OAuth2Client) Context() context.Context {
+	if c.ctx == nil {
+		c.ctx  = context.TODO()
+	}
+	return c.ctx
+}
+
+func (c *OAuth2Client) HTTP(t *oauth2.Token) *http.Client {
+	return c.cfg.Client(c.Context(), t)
+}
+
+func (c *OAuth2Client) ClientID() string {
+	return c.cfg.ClientID
+}
+
+func (c *OAuth2Client) ClientSecret() string {
+	return c.cfg.ClientSecret
+}
+
+func (c *OAuth2Client) TokenURL() string {
+	return c.cfg.Endpoint.TokenURL
+}
+
+func (c *OAuth2Client) AuthURL() string {
+	return c.cfg.Endpoint.AuthURL
+}
+
+func (c *OAuth2Client) Scopes() []string {
+	return c.cfg.Scopes
+}
+
+func (c *OAuth2Client) Domain() string {
+	return c.domain
 }
