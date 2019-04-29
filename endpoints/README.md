88c617478acbd2a4255d110f0b26eb8d9602ad12
autom8ter <admin@autom8ter.com>
2019-04-29 16:17:45 -0600

    endpoints

---
+++ endpoints/README.md
@@ -0,0 +1,120 @@
+# endpoints
+--
+    import "github.com/autom8ter/auth0/endpoints"
+
+
+## Usage
+
+#### func  AuthURL
+
+```go
+func AuthURL(domain string) string
+```
+
+#### func  ClientGrantsURL
+
+```go
+func ClientGrantsURL(domain string) string
+```
+
+#### func  ClientsURL
+
+```go
+func ClientsURL(domain string) string
+```
+
+#### func  ConnectionsURL
+
+```go
+func ConnectionsURL(domain string) string
+```
+
+#### func  CustomDomainsURL
+
+```go
+func CustomDomainsURL(domain string) string
+```
+
+#### func  DeviceCredentials
+
+```go
+func DeviceCredentials(domain string) string
+```
+
+#### func  EmailTemplateURL
+
+```go
+func EmailTemplateURL(domain string) string
+```
+
+#### func  EmailURL
+
+```go
+func EmailURL(domain string) string
+```
+
+#### func  GrantsURL
+
+```go
+func GrantsURL(domain string) string
+```
+
+#### func  JWKSURL
+
+```go
+func JWKSURL(domain string) string
+```
+
+#### func  LogsURL
+
+```go
+func LogsURL(domain string) string
+```
+
+#### func  RolesURL
+
+```go
+func RolesURL(domain string) string
+```
+
+#### func  RulesURL
+
+```go
+func RulesURL(domain string) string
+```
+
+#### func  SearchUsersURL
+
+```go
+func SearchUsersURL(domain string) string
+```
+
+#### func  StatsURL
+
+```go
+func StatsURL(domain string) string
+```
+
+#### func  TenantsURL
+
+```go
+func TenantsURL(domain string) string
+```
+
+#### func  TokenURL
+
+```go
+func TokenURL(domain string) string
+```
+
+#### func  UserInfoURL
+
+```go
+func UserInfoURL(domain string) string
+```
+
+#### func  UsersURL
+
+```go
+func UsersURL(domain string) string
+```
---
+++ endpoints/endpoints.go
@@ -0,0 +1,80 @@
+//go:generate godocdown -o README.md
+
+package endpoints
+
+func TokenURL(domain string) string {
+	return "https://" + domain + "/oauth/token"
+}
+
+func UserInfoURL(domain string) string {
+	return "https://" + domain + "/userinfo"
+}
+
+func UsersURL(domain string) string {
+	return "https://" + domain + "/api/v2/users"
+}
+
+func AuthURL(domain string) string {
+	return "https://" + domain + "/authorize"
+}
+
+func SearchUsersURL(domain string) string {
+	return "https://" + domain + "/api/v2/users-by-email"
+}
+
+func RolesURL(domain string) string {
+	return "https://" + domain + "/api/v2/roles"
+}
+
+func LogsURL(domain string) string {
+	return "https://" + domain + "/api/v2/logs"
+}
+
+func GrantsURL(domain string) string {
+	return "https://" + domain + "/api/v2/grants"
+}
+
+func StatsURL(domain string) string {
+	return "https://" + domain + "/api/v2/stats/active-users"
+}
+
+func ClientsURL(domain string) string {
+
+	return "https://" + domain + "/api/v2/clients"
+}
+
+func ClientGrantsURL(domain string) string {
+	return "https://" + domain + "/api/v2/client-grants"
+}
+
+func ConnectionsURL(domain string) string {
+	return "https://" + domain + "/api/v2/connections"
+}
+
+func CustomDomainsURL(domain string) string {
+	return "https://" + domain + "/api/v2/custom-domains"
+}
+
+func RulesURL(domain string) string {
+	return "https://" + domain + "/api/v2/rules"
+}
+
+func TenantsURL(domain string) string {
+	return "https://" + domain + "/api/v2/tenants/settings"
+}
+
+func EmailURL(domain string) string {
+	return "https://" + domain + "/api/v2/emails/provider"
+}
+
+func EmailTemplateURL(domain string) string {
+	return "https://" + domain + "/api/v2/email-templates"
+}
+
+func DeviceCredentials(domain string) string {
+	return "https://" + domain + "/api/v2/device-credentials"
+}
+
+func JWKSURL(domain string) string {
+	return "https://" + domain + "/.well-known/jwks.json"
+}
